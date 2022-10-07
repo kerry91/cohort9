c4395c0683a98920ecebd8b2a6f741a033378b8c
kerry <kerry@warnes.com>
2022-10-07 18:52:10 +0100

    .

---
+++ session16/functions/script.js
@@ -0,0 +1,109 @@
+//Dark mode
+function toggleDarkMode() {
+  const s1Element = document.getElementById("s1-div");
+  const s1Element2 = document.getElementById("s1-h1");
+  s1Element.classList.toggle("dark-mode");
+  s1Element2.classList.toggle("dark-mode");
+}
+
+//Slideshow
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
+
+//Accordion
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+
+//Scroll animation
+const scrollElements = document.querySelectorAll(".js-scroll");
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.25)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      hideScrollElement(el)
+    }
+  })
+}
+
+window.addEventListener("scroll", () => {
+  handleScrollAnimation();
+});
+
+
+//Form submit
+document.getElementById("myForm").addEventListener("submit", submitForm());
+function submitForm() {
+
+  let fn = document.getElementById("fname").value;
+  let ln = document.getElementById("lname").value;
+  const result = document.getElementById("results");
+
+  if (fn === "") {
+    result.innerHTML = "Enter a first name";
+  }
+  else if (ln === "") {
+    result.innerHTML = "Enter a last name";
+  }
+  else {
+    result.innerHTML = `${fn}` + " " + `${ln}`;
+  }
+}
+
