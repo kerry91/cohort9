cd9bbdc2f07a621399fbcdf1b0a3497041616a43
KAW <36702039+kerry91@users.noreply.github.com>
2022-11-07 01:59:06 +0000

    .

---
+++ Final/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Main from './components/Main';
+import './styles.css';
+
+function App() {
+  return (
+    <>
+      <Main/>
+    </>
+  );
+}
+
+export default App;
